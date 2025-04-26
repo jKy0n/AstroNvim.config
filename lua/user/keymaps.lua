8a7c83ff3263b2f9f02ccf0de449621a23e03d0c
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2025-04-26 20:15:51 -0300

    remove: jkyon-shortcuts keybinds && add: keymaps && mod: configure avante

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,5 +1,9 @@
+-- ~/.config/nvim/lua/user/keymaps.lua
+
+-- Opções padrão para os keymaps
 local opts = { noremap = true, silent = true }
 
+-- Keybinds Gerais
 -- Navegar entre painéis usando Ctrl + Setas
 vim.keymap.set('n', '<C-Left>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-Down>', '<C-w>j', opts)
@@ -28,3 +32,16 @@ vim.keymap.set("i", "<S-Del>", function()
   vim.cmd("normal! 0dd")
   vim.cmd("startinsert")
 end, { desc = "Delete line (Shift+Del) [insert mode]" })
+
+-- Which-key: Atalhos Rápidos
+local which_key = require("which-key")
+which_key.register({
+  ["<leader>h"] = {
+    name = "Atalhos Rápidos",
+    u = { ":undo<CR>", "Undo" },
+    r = { ":redo<CR>", "Redo" },
+    s = { ":w<CR>", "Salvar" },
+    q = { ":q<CR>", "Fechar" },
+  },
+})
+
