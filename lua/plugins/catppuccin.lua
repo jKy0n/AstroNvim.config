7187e153a1c66e4b5d4aeae0790e80ca18c305ae
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2025-04-27 02:45:36 -0300

    catppuccin: statusbar non-transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,12 @@ return {
     name = "catppuccin",
     opts = {
         flavour = "macchiato",
-        transparent_background = true
+        transparent_background = true,
+        custom_highlights = function(colors)
+            return {
+                StatusLine = { bg = colors.mantle }, -- ou colors.base, como preferir
+                StatusLineNC = { bg = colors.mantle }, -- Linha de status quando a janela não está focada
+        }
+        end,
     },
 }
